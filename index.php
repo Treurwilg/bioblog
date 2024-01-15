0df731c78f3246af236566c065a9ed2d556514b5
Jan Kooijman <jcz.kooijman@hetnet.nl>
2024-01-15 10:37:19 +0100

    Added my project

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
+<?php require_once( ROOT_PATH . '/includes/head_section.php') ?>
+	<title>LifeBlog | Home </title>
+</head>
+<body>
+	<!-- container - wraps whole page -->
+	<div class="container">
+		<!-- navbar -->
+		<?php include( ROOT_PATH . '/includes/navbar.php') ?>
+		<!-- // navbar -->
+		
+		<!-- banner -->
+		<?php include( ROOT_PATH . '/includes/banner.php') ?>
+		<!-- // banner -->
+		
+		<!-- Retrieve all posts from database -->
+		<?php $posts = getPublishedPosts(); ?>
+		
+		<!-- Page content -->/.
+		<div class="content">
+			<h2 class="content-title">Recent Articles</h2>
+			<hr> 
+			<!-- more content still to come here, -->
+			<?php foreach ($posts as $post): ?>
+				<div class="post" style="margin-left: 0px;">
+					<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+					<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+						<div class="post_info">
+							<h3><?php echo $post['title'] ?></h3>
+							<div class="info">
+								<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+								<span class="read_more">Read more...</span>							
+							</div>					
+						</div>			
+					</a>
+				</div>
+			<?php endforeach; ?>
+		<!-- // Page content -->
+
+		
+		<!-- footer -->
+		<?php require_once( ROOT_PATH . '/includes/footer.php') ?>
+		<!-- // footer -->
+	
