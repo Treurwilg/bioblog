97351fc4b633256fe5d33410ebee1e58aa35c048
Jan Kooijman <jcz.kooijman@hetnet.nl>
2024-08-26 12:35:19 +0200

    Hazelaar: vervang oude voor herstart

--- includes/head_section.php
+++ includes/head_section.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine"
 	rel="stylesheet">
 	<!-- Styling for public area -->
 	<link rel="stylesheet" href="static/css/public_styling.css">
-	
+	<meta charset="UTF-8">
