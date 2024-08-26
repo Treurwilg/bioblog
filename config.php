805a2c583324b1cd0ab45bd3fd9124185cc1212b
Jan Kooijman <jcz.kooijman@hetnet.nl>
2024-08-26 15:23:08 +0200

    Hazelaar: definieer ROOT_PATH en BASE_URL

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
-<?php // new
+<?php
 	session_start();
 	// connect to database
-	$conn = mysqli_connect("ict4us-blogs.db.transip.me", "ict4us_bio", "7q9uVtg6aRaxFBAj3tiPyxBzDGT95qPG8rBe", "ict4us_blogs");
-	if (!$conn ) {die("Error connecting to database: " . mysqli_connect_error());}
+	// $conn = mysqli_connect("ict4us-blogs.db.transip.me", "ict4us_bio", "7q9uVtg6aRaxFBAj3tiPyxBzDGT95qPG8rBe", "ict4us_blogs");
+	// if (!$conn ) {die("Error connecting to database: " . mysqli_connect_error());}
 	
 	// define global constants
 	define('ROOT_PATH', realpath(dirname(__FILE__)));
-	define('BASE_URL', 'https://ict4us.nl/bioblog/');
+	define('BASE_URL', 'https://localhost/bioblog/');
 ?>
